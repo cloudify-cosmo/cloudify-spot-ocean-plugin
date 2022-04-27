8c61cc47fc1aad41c695d49e0422cc88c60cb3df
kaplanyaniv <yaniv.224@gmail.com>
2022-04-27 14:12:25 +0300

    setting up repo

---
+++ setup.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2018 Cloudify Platform Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_file='plugin.yaml'):
+    lines = read(rel_file)
+    for line in lines.splitlines():
+        if 'package_version' in line:
+            split_line = line.split(':')
+            line_no_space = split_line[-1].replace(' ', '')
+            line_no_quotes = line_no_space.replace('\'', '')
+            return line_no_quotes.strip('\n')
+    raise RuntimeError('Unable to find version string.')
+
+
+setup(
+    name='cloudify-spot-ocean-plugin',
+    version=get_version(),
+    author='Cloudify Platform Ltd.',
+    author_email='hello@cloudify.co',
+    license='LICENSE',
+    packages=find_packages(exclude=['tests*']),
+    description='A Cloudify plugin for Spot Ocean',
+    install_requires=[
+        'boto3',
+        'cloudify-common>=4.5',
+        'cloudify-utilities-plugins-sdk>=0.0.61',
+        'botocore',
+        'pycryptodome==3.9.7',
+        'deepdiff==3.3.0'
+    ]
+)
