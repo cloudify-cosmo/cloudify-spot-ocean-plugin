7b8d4529e59139952da513f9f405cfb3df471193
kaplanyaniv <yaniv.224@gmail.com>
2022-06-20 10:44:19 +0300

    removed unnecessary files

--- cloudify_spot_ocean/get_all_ocean_cluster_script.py
+++
@@ -1,8 +0,0 @@
-from spotinst_sdk2 import SpotinstSession
-from spotinst_sdk2.models.ocean import aws
-
-def describe_all(token=None, account=None):
-	session = SpotinstSession(auth_token=token, account_id=account)
-	client = session.client("ocean_aws")
-
-	return client.get_all_ocean_cluster()
