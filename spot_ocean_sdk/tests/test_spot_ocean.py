5acd790e783575165039a8951a6cdf8a56c60cb3
kaplanyaniv <yaniv.224@gmail.com>
2022-07-04 14:43:30 +0300

    updated tests

--- spot_ocean_sdk/tests/test_spot_ocean.py
+++ spot_ocean_sdk/tests/test_spot_ocean.py
@@ -3,14 +3,14 @@ from mock import Mock, patch
 from spotinst_sdk2.models.ocean import aws
 
 RESOURCE_CONFIG = {
-	'InstanceTypes': 't3.medium',
-    'OceanClusterName': None,
-    'ClusterID': None,
-    'Region':  None,
-    'SecurityGroupIDs': None,
-    'ImageID': None,
-    'KeyPair': None,
-    'SubnetIDs': None,
+    'InstanceTypes': 't3.medium',
+    'OceanClusterName': 'eks-name',
+    'ClusterID': 'cluster-1111',
+    'Region': 'us-west-1',
+    'SecurityGroupIDs': ['sg-1'],
+    'ImageID': 'ami-1',
+    'KeyPair': 'keypair',
+    'SubnetIDs': ['subnet-1'],
     'MinCapacity': 1,
     'MaxCapacity': 1,
     'TargetCapacity': 1
@@ -36,10 +36,8 @@ def test_get_capacity_object(spot_ocean_aws, *_, **__):
 
 @patch('spot_ocean_sdk.spot_ocean.aws')
 def test_get_instance_types_object(spot_ocean_aws, *_, **__):
-    # spot_ocean_aws.InstanceTypes = Mock()
     spot_ocean_aws.InstanceTypes = aws.InstanceTypes(whitelist=[
         RESOURCE_CONFIG.get('InstanceTypes')])
-    # spot_ocean_aws.InstanceTypes.client = Mock()
     instance_types = spot_ocean.get_instance_types_object(RESOURCE_CONFIG.get(
         'InstanceTypes'))
     assert instance_types.whitelist == spot_ocean_aws.InstanceTypes.whitelist
@@ -61,4 +59,54 @@ def test_get_strategy_object(spot_ocean_aws, *_, **__):
     assert strategy.fallback_to_od == spot_ocean_aws.Strategy.fallback_to_od
     assert strategy.spot_percentage == spot_ocean_aws.Strategy.spot_percentage
 
+
+@patch('spot_ocean_sdk.spot_ocean.aws')
+def test_get_launch_specification_object(spot_ocean_aws, *_, **__):
+    spot_ocean_aws.LaunchSpecifications = aws.LaunchSpecifications(
+        security_group_ids=RESOURCE_CONFIG.get('SecurityGroupIDs'),
+        image_id=RESOURCE_CONFIG.get('ImageID'),
+        key_pair=RESOURCE_CONFIG.get('KeyPair'))
+    launch_specification = spot_ocean.get_launch_specification_object(
+        security_group_ids=RESOURCE_CONFIG.get('SecurityGroupIDs'),
+        image_id=RESOURCE_CONFIG.get('ImageID'),
+        key_pair=RESOURCE_CONFIG.get('KeyPair'))
+    assert launch_specification.security_group_ids == \
+           spot_ocean_aws.LaunchSpecifications.security_group_ids
+    assert launch_specification.image_id == \
+           spot_ocean_aws.LaunchSpecifications.image_id
+    assert launch_specification.key_pair == \
+           spot_ocean_aws.LaunchSpecifications.key_pair
+
+
+def test_get_compute_object(spot_ocean_aws, *_, **__):
+    spot_ocean_aws.Compute = aws.Compute(
+        instance_types=spot_ocean_aws.InstanceTypes,
+        subnet_ids=RESOURCE_CONFIG.get('SubnetIDs'),
+        launch_specification=spot_ocean_aws.LaunchSpecifications)
+    compute = spot_ocean.get_compute_object(
+        instance_types=spot_ocean_aws.InstanceTypes,
+        subnet_ids=RESOURCE_CONFIG.get('SubnetIDs'),
+        launch_specification=spot_ocean_aws.LaunchSpecifications)
+    assert compute.subnet_ids == spot_ocean_aws.Compute.subnet_ids
+
+
+def test_get_ocean_object(spot_ocean_aws, *_, **__):
+    spot_ocean_aws.Ocean = aws.Ocean(
+        name=RESOURCE_CONFIG.get('OceanClusterName'),
+        controller_cluster_id=RESOURCE_CONFIG.get('ClusterID'),
+        region=RESOURCE_CONFIG.get('Region'),
+        capacity=spot_ocean_aws.Capacity,
+        strategy=spot_ocean_aws.Strategy,
+        compute=spot_ocean_aws.Compute)
+    ocean = spot_ocean.get_ocean_object(
+        name=RESOURCE_CONFIG.get('OceanClusterName'),
+        controller_cluster_id=RESOURCE_CONFIG.get('ClusterID'),
+        region=RESOURCE_CONFIG.get('Region'),
+        capacity=spot_ocean_aws.Capacity,
+        strategy=spot_ocean_aws.Strategy,
+        compute=spot_ocean_aws.Compute)
+    assert ocean.name == spot_ocean_aws.Ocean.name
+    assert ocean.controller_cluster_id == \
+           spot_ocean_aws.Ocean.controller_cluster_id
+    assert ocean.region == spot_ocean_aws.Ocean.region
 
