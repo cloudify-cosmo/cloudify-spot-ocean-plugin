c6d5da97d1a10056ff6537486f28629875fab4db
kaplanyaniv <yaniv.224@gmail.com>
2022-07-05 11:06:11 +0300

    updated tests

--- spot_ocean_sdk/tests/test_spot_ocean.py
+++ spot_ocean_sdk/tests/test_spot_ocean.py
@@ -19,94 +19,119 @@ RESOURCE_CONFIG = {
 
 @patch('spot_ocean_sdk.spot_ocean.aws')
 def test_get_capacity_object(spot_ocean_aws, *_, **__):
-    min = RESOURCE_CONFIG.get('MinCapacity')
-    max = RESOURCE_CONFIG.get('MaxCapacity')
-    target = RESOURCE_CONFIG.get('TargetCapacity')
-    spot_ocean_aws.Capacity = aws.Capacity(minimum=min,
-                                           maximum=max,
-                                           target=target)
 
-    capacity = spot_ocean.get_capacity_object(minimum=min,
-                                              maximum=max,
-                                              target=target)
-    assert capacity.minimum == spot_ocean_aws.Capacity.minimum
-    assert capacity.maximum == spot_ocean_aws.Capacity.maximum
-    assert capacity.target == spot_ocean_aws.Capacity.target
+    class MockCap(object):
+        def __init__(self,
+                     minimum,
+                     maximum,
+                     target):
+            self.minimum = minimum
+            self.maximum = maximum
+            self.target = target
+    spot_ocean_aws.Capacity = MockCap
+
+    capacity = spot_ocean.get_capacity_object(
+        minimum=RESOURCE_CONFIG.get('MinCapacity'),
+        maximum=RESOURCE_CONFIG.get('MaxCapacity'),
+        target=RESOURCE_CONFIG.get('TargetCapacity'))
+    assert capacity.minimum == RESOURCE_CONFIG.get('MinCapacity')
+    assert capacity.maximum == RESOURCE_CONFIG.get('MaxCapacity')
+    assert capacity.target == RESOURCE_CONFIG.get('TargetCapacity')
 
 
 @patch('spot_ocean_sdk.spot_ocean.aws')
 def test_get_instance_types_object(spot_ocean_aws, *_, **__):
-    spot_ocean_aws.InstanceTypes = aws.InstanceTypes(whitelist=[
-        RESOURCE_CONFIG.get('InstanceTypes')])
+    class MockIT(object):
+        def __init__(self, whitelist):
+            self.whitelist = whitelist
+
+    spot_ocean_aws.InstanceTypes = MockIT
     instance_types = spot_ocean.get_instance_types_object(RESOURCE_CONFIG.get(
         'InstanceTypes'))
-    assert instance_types.whitelist == spot_ocean_aws.InstanceTypes.whitelist
-    assert instance_types.blacklist == spot_ocean_aws.InstanceTypes.blacklist
+    assert instance_types.whitelist == [RESOURCE_CONFIG.get('InstanceTypes')]
 
 
 @patch('spot_ocean_sdk.spot_ocean.aws')
 def test_get_strategy_object(spot_ocean_aws, *_, **__):
-    spot_ocean_aws.Strategy = aws.Strategy(
-        utilize_reserved_instances=False,
-        fallback_to_od=True,
-        spot_percentage=100)
-    print(spot_ocean_aws.Strategy)
+    class MockS(object):
+        def __init__(self,
+                     utilize_reserved_instances,
+                     fallback_to_od,
+                     spot_percentage):
+            self.utilize_reserved_instances = utilize_reserved_instances
+            self.fallback_to_od = fallback_to_od
+            self.spot_percentage = spot_percentage
+
+    spot_ocean_aws.Strategy = MockS
 
     strategy = spot_ocean.get_strategy_object()
-    print(strategy)
-    assert strategy.utilize_reserved_instances == \
-           spot_ocean_aws.Strategy.utilize_reserved_instances
-    assert strategy.fallback_to_od == spot_ocean_aws.Strategy.fallback_to_od
-    assert strategy.spot_percentage == spot_ocean_aws.Strategy.spot_percentage
+
+    assert not strategy.utilize_reserved_instances  # value is set to False
+    assert strategy.fallback_to_od  # value is set to True
+    assert strategy.spot_percentage == 100  # value is set to 100%
 
 
 @patch('spot_ocean_sdk.spot_ocean.aws')
 def test_get_launch_specification_object(spot_ocean_aws, *_, **__):
-    spot_ocean_aws.LaunchSpecifications = aws.LaunchSpecifications(
-        security_group_ids=RESOURCE_CONFIG.get('SecurityGroupIDs'),
-        image_id=RESOURCE_CONFIG.get('ImageID'),
-        key_pair=RESOURCE_CONFIG.get('KeyPair'))
+    class MockLS(object):
+        def __init__(self, security_group_ids, image_id, key_pair):
+            self.security_group_ids = security_group_ids
+            self.image_id = image_id
+            self.key_pair = key_pair
+
+    spot_ocean_aws.LaunchSpecifications = MockLS
+
     launch_specification = spot_ocean.get_launch_specification_object(
         security_group_ids=RESOURCE_CONFIG.get('SecurityGroupIDs'),
         image_id=RESOURCE_CONFIG.get('ImageID'),
         key_pair=RESOURCE_CONFIG.get('KeyPair'))
     assert launch_specification.security_group_ids == \
-           spot_ocean_aws.LaunchSpecifications.security_group_ids
-    assert launch_specification.image_id == \
-           spot_ocean_aws.LaunchSpecifications.image_id
-    assert launch_specification.key_pair == \
-           spot_ocean_aws.LaunchSpecifications.key_pair
+           RESOURCE_CONFIG.get('SecurityGroupIDs')
+    assert launch_specification.image_id == RESOURCE_CONFIG.get('ImageID')
+    assert launch_specification.key_pair == RESOURCE_CONFIG.get('KeyPair')
 
 
+@patch('spot_ocean_sdk.spot_ocean.aws')
 def test_get_compute_object(spot_ocean_aws, *_, **__):
-    spot_ocean_aws.Compute = aws.Compute(
-        instance_types=spot_ocean_aws.InstanceTypes,
-        subnet_ids=RESOURCE_CONFIG.get('SubnetIDs'),
-        launch_specification=spot_ocean_aws.LaunchSpecifications)
+    class MockComp(object):
+        def __init__(self, instance_types, subnet_ids, launch_specification):
+            self.instance_types = instance_types
+            self.subnet_ids = subnet_ids
+            self.launch_specification = launch_specification
+
+    spot_ocean_aws.Compute = MockComp
     compute = spot_ocean.get_compute_object(
         instance_types=spot_ocean_aws.InstanceTypes,
         subnet_ids=RESOURCE_CONFIG.get('SubnetIDs'),
         launch_specification=spot_ocean_aws.LaunchSpecifications)
-    assert compute.subnet_ids == spot_ocean_aws.Compute.subnet_ids
+    assert compute.subnet_ids == RESOURCE_CONFIG.get('SubnetIDs')
 
 
+@patch('spot_ocean_sdk.spot_ocean.aws')
 def test_get_ocean_object(spot_ocean_aws, *_, **__):
-    spot_ocean_aws.Ocean = aws.Ocean(
-        name=RESOURCE_CONFIG.get('OceanClusterName'),
-        controller_cluster_id=RESOURCE_CONFIG.get('ClusterID'),
-        region=RESOURCE_CONFIG.get('Region'),
-        capacity=spot_ocean_aws.Capacity,
-        strategy=spot_ocean_aws.Strategy,
-        compute=spot_ocean_aws.Compute)
+    class MockO(object):
+        def __init__(self,
+                     name,
+                     controller_cluster_id,
+                     region,
+                     capacity,
+                     strategy,
+                     compute):
+            self.name = name
+            self.controller_cluster_id = controller_cluster_id
+            self.region = region
+            self.capacity = capacity
+            self.strategy = strategy
+            self.compute = compute
+
+    spot_ocean_aws.Ocean = MockO
     ocean = spot_ocean.get_ocean_object(
         name=RESOURCE_CONFIG.get('OceanClusterName'),
-        controller_cluster_id=RESOURCE_CONFIG.get('ClusterID'),
+        cluster_id=RESOURCE_CONFIG.get('ClusterID'),
         region=RESOURCE_CONFIG.get('Region'),
         capacity=spot_ocean_aws.Capacity,
         strategy=spot_ocean_aws.Strategy,
         compute=spot_ocean_aws.Compute)
-    assert ocean.name == spot_ocean_aws.Ocean.name
-    assert ocean.controller_cluster_id == \
-           spot_ocean_aws.Ocean.controller_cluster_id
-    assert ocean.region == spot_ocean_aws.Ocean.region
-
+    assert ocean.name == RESOURCE_CONFIG.get('OceanClusterName')
+    assert ocean.controller_cluster_id == RESOURCE_CONFIG.get('ClusterID')
+    assert ocean.region == RESOURCE_CONFIG.get('Region')
