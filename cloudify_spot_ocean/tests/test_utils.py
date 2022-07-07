5d344a106336deaf0ce610604f9a909b3c4cef49
kaplanyaniv <yaniv.224@gmail.com>
2022-07-07 16:05:07 +0300

    finished decorator tests & started task tests

---
+++ cloudify_spot_ocean/tests/test_utils.py
@@ -0,0 +1,39 @@
+from . import mock_context
+from .. import utils
+
+
+def test_get_resource_config():
+    mock_context(test_name="test_decorator_stores_kwargs",
+                 test_node_id="test_decorator_stores_kwargs",
+                 test_properties={
+                     'client_config': {'spot_ocean_token': 'tok-1',
+                                       'account_id': 'act-1'},
+                     'resource_config':
+                         {'resource_config': 'resource_config'}},
+                 test_runtime_properties={})
+    resource_config = utils.get_resource_config()
+    assert resource_config == {'resource_config': 'resource_config'}
+
+
+def test_get_client_config():
+    mock_context(test_name="test_decorator_stores_kwargs",
+                 test_node_id="test_decorator_stores_kwargs",
+                 test_properties={
+                     'client_config': {'spot_ocean_token': 'tok-1',
+                                       'account_id': 'act-1'},
+                     'resource_config':
+                         {'resource_config': 'resource_config'}},
+                 test_runtime_properties={})
+    client_config = utils.get_client_config()
+    assert client_config == {'spot_ocean_token': 'tok-1',
+                             'account_id': 'act-1'}
+
+
+def test_validate_resource_config():
+    a = {'foo': "bar", 'gpp': 'nar', 'buz': 'zun'}
+    b = {'foo': "bar", 'gpp': 'nar', 'buz': 'zun'}
+    c = {'foo': "bar", 'buz': 'zun'}
+
+    assert utils.validate_resource_config(a, b)
+    assert utils.validate_resource_config(b, c)
+    assert not utils.validate_resource_config(c, b)
